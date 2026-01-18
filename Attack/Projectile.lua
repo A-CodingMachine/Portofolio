63a5bb99e19bf3aa2ee6606ffcc7ec632d640f50
A-CodingMachine <132759762+A-CodingMachine@users.noreply.github.com>
2026-01-18 04:02:28 +0200

    Update Projectile.lua

--- Attack/Projectile.lua
+++ Attack/Projectile.lua
@@ -1,122 +1,263 @@
---Services
+--// Services
 local RStorage = game:GetService("ReplicatedStorage")
 local SPlayer = game:GetService("Players")
 local SRun = game:GetService("RunService")
+local STween = game:GetService("TweenService")
 
---Modules
+--// Modules
 local FastCast = require(RStorage.QOL.FastCastRedux)
 local Peripherals = require("./Peripherals")
 local AnimationLoader = require(RStorage.QOL.AnimationLoader)
+local ProjectileInfo = require(RStorage.Info.ProjectileInfo)
+local CameraShaker = require(RStorage.QOL.CameraShaker)
 
---Var
+--// Vars
 local LPlayer = SPlayer.LocalPlayer
 local Character = LPlayer.Character
+local Camera = workspace.Camera
 
+--//BodyParts
+local torso = Character:WaitForChild("Torso")
+local shoulder = torso:WaitForChild("Right Shoulder") 
 
---Animations
-local Throw = AnimationLoader.ReturnAnimation(AnimationLoader.Animations[LPlayer.Weapon.Value].Throw, Character.Humanoid.Animator)
+--// Gui
+local ChargeBar = RStorage.GUI.ChargeBar
 
+--// Animations
+local Throw = AnimationLoader.ReturnAnimation(
+	AnimationLoader.Animations.Pin.Throw,
+	Character.Humanoid.Animator
+)
 
---FastCastSettings
+--// FastCast Settings
 local Caster = FastCast.new()
 local Behavior = FastCast.newBehavior()
 
 local Params = RaycastParams.new()
 Params.FilterType = Enum.RaycastFilterType.Exclude
-Params.FilterDescendantsInstances = {LPlayer.Character}
+Params.FilterDescendantsInstances = { LPlayer.Character }
 
 Behavior.RaycastParams = Params
-Behavior.Acceleration = Vector3.new(0,-50,0)
-Behavior.CosmeticBulletTemplate = RStorage.Bullet
+Behavior.Acceleration = Vector3.new(0, -40, 0)
+Behavior.CosmeticBulletTemplate = RStorage.KITS.Projectiles.Bullet
 Behavior.CosmeticBulletContainer = workspace
 
+--// Main Module
 local Main = {}
---Settings
-Main._HoldLength = 0
-Main.Direction = nil
-Main.TrajectoryConnection = nil
-Main.LastProjectile = tick() - 10
-Main.ProjectileCooldown = 2
 
-function Main.CreateProjectile(StartPosition, Dir, Velocity, Object, Creator)
-	if Creator == LPlayer then print('Same as chaaracter'); return end
+-- Settings
+Main._HoldLength = 0
+Main._OriginalC0 = 0
+Main._Direction = nil
+Main._TrajectoryConnection = nil
+Main._LastProjectile = tick() - 10
+Main._NewShake = nil
+Main._Settings = ProjectileInfo.Default
+
+
+Main.ProjectileCooldown = 2
+Main.MaximumHold = 4
+Main.MaxArmRotationAngle = math.rad(230)
+
+--// 
+
+function Main.CreateProjectile(startPos, dir, velocity, object, creator)
+	if creator == LPlayer then
+		print("Same as character")
+		return
+	end
+
 	Caster = FastCast.new()
 	Behavior = FastCast.newBehavior()
 
-	Behavior.CosmeticBulletTemplate = Object
+	Behavior.CosmeticBulletTemplate = object
 	Behavior.CosmeticBulletContainer = workspace
-	Behavior.Acceleration = Vector3.new(0,-50,0)
+	Behavior.Acceleration = Vector3.new(0, -90, 0)
+	print(velocity)
 
+	local bullet = nil
+	local prevPoint = startPos
 
-	local Bullet = nil
-	local PreviousPoint = StartPosition
+	Caster:Fire(startPos, dir, velocity, Behavior)
 
-	Caster:Fire(StartPosition, Dir,Velocity, Behavior)
+	Caster.LengthChanged:Connect(function(_, lastPoint, rayDir, displacement, _, cosmeticBullet)
+		local newPos = lastPoint + (rayDir * displacement)
+		
+		if not cosmeticBullet then return end
+		
+		cosmeticBullet.PrimaryPart.CFrame = CFrame.lookAt(
+			newPos,
+			newPos + (newPos - prevPoint)
+		)
+		prevPoint = newPos
 
-
-	Caster.LengthChanged:Connect(function(casterThatFired, lastPoint, rayDir, displacement, segmentVelocity, cosmeticBulletObject)
-		cosmeticBulletObject.PrimaryPart.CFrame = CFrame.lookAt(lastPoint + (rayDir * displacement), 
-			lastPoint + (rayDir * displacement) + (lastPoint + (rayDir * displacement) - PreviousPoint) )
-		PreviousPoint = lastPoint + (rayDir * displacement)
-
-		--You can see how the projectile moves with this block of code
-		--workspace.CurrentCamera.CameraType = Enum.CameraType.Scriptable
-		--workspace.CurrentCamera.CFrame = CFrame.lookAt(lastPoint + (rayDir * displacement) + Vector3.new(-4,0,0), lastPoint + (rayDir * displacement))
-
-
-		if not Bullet then Bullet = cosmeticBulletObject end
+		if not bullet then
+			bullet = cosmeticBullet
+		end
 	end)
-	Caster.RayHit:Connect(function( casterThatFired,  result,  segmentVelocity,  cosmeticBulletObject)
+	
+	print("Important BUULLET", bullet)
+
+	Caster.RayHit:Connect(function(_, result)
 		print(result)
+		if bullet then
+			bullet:Destroy()
+		end
 	end)
 
 	Caster.CastTerminating:Connect(function()
 		print("Cast Terminating")
-		Bullet:Destroy()
-	end)
-end
-
-
-function Main.Projectile(isMobile, UpdateProjectileTrajectory)  --Can't require script 
-	if not (tick() - Main.LastProjectile > Main.ProjectileCooldown + 0.5) then return end 
-	Main._HoldLength = tick()
-
-	if Main.TrajectoryConnection then 
-		Main.TrajectoryConnection:Disconnect(); Main.TrajectoryConnection = nil
-
-		for i,v in workspace.TrajectoryPartsFolder:GetChildren() do
-			if v:IsA("BasePart") then v:Destroy() end
+		if bullet then
+			bullet:Destroy()
 		end
-	end	
-	
-	Main.Direction = nil
-	Main.TrajectoryConnection = SRun.Heartbeat:Connect(function()	
-		if isMobile then Main.Direction = UpdateProjectileTrajectory() or Main.Direction end
-		
-		Peripherals.ShowExpectedTrajectory(Vector3.new(0,-50,0), 15 + math.clamp(tick() - Main._HoldLength, 0, 1.5)/1.5 * 70, Main.Direction)
 	end)
-	
 end
 
-function Main.FireProjectile()
-	if not Main.TrajectoryConnection then return end
-	Throw:Play()
-	task.wait(Throw.Length - 0.1)
-	
-	Main.LastProjectile = tick()
-	Main.TrajectoryConnection:Disconnect(); Main.TrajectoryConnection = nil
-
-	for i,v in workspace.TrajectoryPartsFolder:GetChildren() do
-		if v:IsA("BasePart") then v:Destroy() end
+function Main.ChargeBar()
+	if not Character.HumanoidRootPart:FindFirstChild("ChargeBar") then
+		local clone = ChargeBar:Clone()
+		clone.Parent = Character.HumanoidRootPart
+		clone.Adornee = Character.HumanoidRootPart
 	end
 
+	local mainFrame = Character.HumanoidRootPart.ChargeBar.Top
+	local y = math.clamp(Main._HoldLength, 0, Main.MaximumHold) / Main.MaximumHold
+	local tween = STween:Create(
+		mainFrame,
+		TweenInfo.new(0.5, Enum.EasingStyle.Quart),
+		{ Size = UDim2.fromScale(1, y) }
+	)
+	tween:Play()
+end
 
-	local Mouse = LPlayer:GetMouse().Hit.Position
-	local Dir = Main.Direction or (Mouse - Character["Right Arm"].Projectile.WorldPosition).Unit
+function Main.Projectile(isMobile, updateTrajectory)
+	if not (tick() - Main._LastProjectile > Main.ProjectileCooldown + 0.5) then
+		return false
+	end
+	
+	--// Starting Shake Procedure
+	Main._NewShake = CameraShaker.new(2, function(newcf)
+		Camera.CFrame = Camera.CFrame * newcf
+	end)
+	Main._NewShake:Start()
+	Main._NewShake:StartShake(0.5, 12, 1.5)
 
-	task.spawn(Main.CreateProjectile, Character["Right Arm"].Projectile.WorldPosition, Dir, 15 + math.clamp(tick()-Main._HoldLength, 0, 1.5)/1.5 * 70, RStorage.Bullet)
+	if Character:GetAttribute("AbilityEnabled") and ProjectileInfo[LPlayer.Balloon.Value] then
+		Main._Settings = ProjectileInfo[LPlayer.Balloon.Value]
+	else
+		Main._Settings = ProjectileInfo.Default
+	end
+	
+	--// Destroying Trajectory Parts
+	if Main._TrajectoryConnection then
+		Main._TrajectoryConnection:Disconnect()
+		Main._TrajectoryConnection = nil
 
-	RStorage.Remotes.ShootRemote:FireServer(math.clamp(tick()-Main._HoldLength, 0, 1.5), LPlayer:GetMouse().Hit.Position)
+		for _, v in workspace.TrajectoryPartsFolder:GetChildren() do
+			if v:IsA("BasePart") then
+				v:Destroy()
+			end
+		end
+	end
+	
+
+	--// Creating a new Trajectory Connection
+	Main._OriginalC0 = shoulder.C0
+	print(Main._OriginalC0)
+	
+	Main._Direction = nil
+	Main._HoldLength = 0
+	Main._TrajectoryConnection = SRun.PostSimulation:Connect(function(deltaTime)
+		if isMobile then
+			Main._Direction = updateTrajectory(deltaTime) or Main._Direction
+		end
+		
+		Main._HoldLength = deltaTime + Main._HoldLength
+		local percentage = math.clamp(
+			Main._HoldLength,
+			0,
+			Main.MaximumHold
+		) / Main.MaximumHold
+
+		Main.ChargeBar()
+
+		Peripherals.ShowExpectedTrajectory(
+			Vector3.new(0, -90, 0),
+			15 + percentage * Main._Settings.MaximumVelocity,
+			Main._Direction
+		)
+	end)
+
+	return true
+end
+
+function Main.FireProjectile(isMobile)
+	if not Main._TrajectoryConnection then
+		return
+	elseif Main._TrajectoryConnection and isMobile then
+		Main._TrajectoryConnection:Disconnect()
+	end
+	
+ --	torso.TemporaryMotor:Destroy()
+	--shoulder.Enabled = true
+	
+	local Bullet = RStorage.KITS.Projectiles.Bullet
+
+	
+	Throw:Play()
+	task.wait(Throw.Length - 0.1)
+
+	Character.HumanoidRootPart.ChargeBar:Destroy()
+	if Main._TrajectoryConnection then
+		Main._TrajectoryConnection:Disconnect()
+	end
+	
+	Main._NewShake:Stop()
+	Main._LastProjectile = tick()
+	Main._TrajectoryConnection = nil
+
+	for _, v in workspace.TrajectoryPartsFolder:GetChildren() do
+		if v:IsA("BasePart") then
+			v:Destroy()
+		end
+	end
+
+	local mousePos = LPlayer:GetMouse().Hit.Position
+	local dir = Main._Direction or (mousePos - Character["Right Arm"].Projectile.WorldPosition).Unit
+	
+	if Character:GetAttribute("AbilityEnabled") and LPlayer.Balloon.Value == "Monkey" then
+		Bullet = RStorage.KITS.Projectiles.Monkey
+	end
+
+	task.spawn(
+		Main.CreateProjectile,
+		Character["Right Arm"].Projectile.WorldPosition,
+		dir,
+		15 + math.clamp(Main._HoldLength, 0, 1.5) / 1.5 * Main._Settings.MaximumVelocity,
+		Bullet
+	)
+
+	RStorage.Remotes.ShootRemote:FireServer(
+		math.clamp(Main._HoldLength, 0, 1.5),
+		dir
+	)
+end
+
+function Main.Cleanup()
+	if Main._TrajectoryConnection then
+		Main._TrajectoryConnection:Disconnect()
+	end
+	
+	if Main._NewShake then Main._NewShake:Stop() end
+	Main._LastProjectile = tick()
+	Main._TrajectoryConnection = nil
+
+	for _, v in workspace.TrajectoryPartsFolder:GetChildren() do
+		if v:IsA("BasePart") then
+			v:Destroy()
+		end
+	end
+	
 end
 
 return Main
